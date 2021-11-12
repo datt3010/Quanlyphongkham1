52ec7990d97d89066464f250887da56f706e9b1b
datt3010 <datthuynh30102002@gmail.com>
2021-11-12 12:06:09 +0700

    HuynhTanDat
    update-quan ly nhan vien

--- QuanLyPhongKham/src/UI/dangnhap.java
+++ QuanLyPhongKham/src/UI/dangnhap.java
@@ -4,13 +4,15 @@
  * and open the template in the editor.
  */
 package UI;
-
+import Model.NhanVien;
+import DAO.nhanvienDAO;
+import Utilities.MsgBox;
 /**
  *
  * @author p
  */
 public class dangnhap extends javax.swing.JFrame {
-
+nhanvienDAO daonv=new nhanvienDAO();
     /**
      * Creates new form dangnhap
      */
@@ -33,16 +35,16 @@ public class dangnhap extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jPanel3 = new javax.swing.JPanel();
-        jLabel6 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
+        lblquenmatkhau = new javax.swing.JLabel();
+        btndoimatkhau = new javax.swing.JButton();
         jSeparator2 = new javax.swing.JSeparator();
         jSeparator1 = new javax.swing.JSeparator();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
+        txtuser = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
-        jPasswordField1 = new javax.swing.JPasswordField();
-        jButton2 = new javax.swing.JButton();
+        txtpassword = new javax.swing.JPasswordField();
+        btndangnhap = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -86,15 +88,15 @@ public class dangnhap extends javax.swing.JFrame {
         jPanel3.setBackground(new java.awt.Color(255, 102, 102));
         jPanel3.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel6.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jLabel6.setText("Quên mật khẩu ?");
-        jPanel3.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 290, -1, -1));
+        lblquenmatkhau.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        lblquenmatkhau.setText("Quên mật khẩu ?");
+        jPanel3.add(lblquenmatkhau, new org.netbeans.lib.awtextra.AbsoluteConstraints(340, 290, -1, -1));
 
-        jButton1.setBackground(new java.awt.Color(255, 153, 153));
-        jButton1.setFont(new java.awt.Font("Segoe UI", 0, 16)); // NOI18N
-        jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icon/icons8-home-page-23.png"))); // NOI18N
-        jButton1.setText("Đổi mật khẩu?");
-        jPanel3.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 330, 190, 50));
+        btndoimatkhau.setBackground(new java.awt.Color(255, 153, 153));
+        btndoimatkhau.setFont(new java.awt.Font("Segoe UI", 0, 16)); // NOI18N
+        btndoimatkhau.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icon/icons8-home-page-23.png"))); // NOI18N
+        btndoimatkhau.setText("Đổi mật khẩu?");
+        jPanel3.add(btndoimatkhau, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 330, 190, 50));
 
         jSeparator2.setBackground(new java.awt.Color(255, 0, 0));
         jPanel3.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 270, 320, 10));
@@ -111,29 +113,29 @@ public class dangnhap extends javax.swing.JFrame {
         jLabel4.setText("Mật khẩu");
         jPanel3.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 250, 77, -1));
 
-        jTextField1.setBackground(new java.awt.Color(255, 102, 102));
-        jTextField1.setBorder(null);
-        jPanel3.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(147, 164, 326, 33));
+        txtuser.setBackground(new java.awt.Color(255, 102, 102));
+        txtuser.setBorder(null);
+        jPanel3.add(txtuser, new org.netbeans.lib.awtextra.AbsoluteConstraints(147, 164, 326, 33));
 
         jLabel5.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
         jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel5.setText("Tài Khoản");
         jPanel3.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(44, 160, -1, 33));
 
-        jPasswordField1.setBackground(new java.awt.Color(255, 102, 102));
-        jPasswordField1.setBorder(null);
-        jPanel3.add(jPasswordField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 250, 326, 33));
+        txtpassword.setBackground(new java.awt.Color(255, 102, 102));
+        txtpassword.setBorder(null);
+        jPanel3.add(txtpassword, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 250, 326, 33));
 
-        jButton2.setBackground(new java.awt.Color(255, 153, 153));
-        jButton2.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
-        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icon/icons8_chart_30px.png"))); // NOI18N
-        jButton2.setText("Đăng nhập");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        btndangnhap.setBackground(new java.awt.Color(255, 153, 153));
+        btndangnhap.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        btndangnhap.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icon/icons8_chart_30px.png"))); // NOI18N
+        btndangnhap.setText("Đăng nhập");
+        btndangnhap.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                btndangnhapActionPerformed(evt);
             }
         });
-        jPanel3.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 330, -1, 50));
+        jPanel3.add(btndangnhap, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 330, -1, 50));
 
         jPanel1.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(510, 0, 550, 550));
 
@@ -153,11 +155,10 @@ public class dangnhap extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void btndangnhapActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btndangnhapActionPerformed
         // TODO add your handling code here:
-        new QuanLyBacSi().setVisible(true);
-        dispose();
-    }//GEN-LAST:event_jButton2ActionPerformed
+        login();
+    }//GEN-LAST:event_btndangnhapActionPerformed
 
     /**
      * @param args the command line arguments
@@ -193,22 +194,37 @@ public class dangnhap extends javax.swing.JFrame {
             }
         });
     }
-
+public void login(){
+    String taikhoan=txtuser.getText();
+    String pass=new String(txtpassword.getPassword()); 
+    NhanVien nhanVien=daonv.SelectByID(taikhoan); 
+    if(nhanVien==null){
+        MsgBox.alert(this,"sai tên đăng nhập");
+    }
+    else if(!pass.equals(nhanVien.getMatkhau())){
+        MsgBox.alert(this,"sai mật khẩu");
+    }
+    else{
+        Utilities.Auths.user=nhanVien; 
+        new QuanLyphongkham().setVisible(true);
+        this.dispose();
+    }
+}
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton btndangnhap;
+    private javax.swing.JButton btndoimatkhau;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
-    private javax.swing.JPasswordField jPasswordField1;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSeparator jSeparator2;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JLabel lblquenmatkhau;
+    private javax.swing.JPasswordField txtpassword;
+    private javax.swing.JTextField txtuser;
     // End of variables declaration//GEN-END:variables
 }
--- QuanLyPhongKham/src/Utilities/MsgBox.java
+++ QuanLyPhongKham/src/Utilities/MsgBox.java
@@ -5,10 +5,25 @@
  */
 package Utilities;
 
+import java.awt.Component;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author OS
  */
 public class MsgBox {
-    
+    // tạo 1 component cho tin nhắn
+    public static void alert(Component parent,String mess){
+        JOptionPane.showMessageDialog(parent, mess,"Phòng khám An Nhiên",JOptionPane.INFORMATION_MESSAGE);
+    }
+    // tạo 1 component để xác nhận
+    public static boolean confirm(Component parent,String mess){
+        int result =JOptionPane.showConfirmDialog(parent, mess,"Phòng khám An Nhiên",JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+        return  result==JOptionPane.YES_OPTION; 
+    }
+    // tạo 1 component cho người dùng nhập
+    public static String prompt(Component parent,String mess){
+        return JOptionPane.showInputDialog(parent,mess,"Phòng khám An Nhiên",JOptionPane.INFORMATION_MESSAGE);
+    }
 }
